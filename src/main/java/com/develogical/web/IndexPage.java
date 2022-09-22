7743e04b550660d9f2698e60f3f49d11d0ab77db
Yuqi Gong <yuqigong@andrew.cmu.edu>
2022-09-22 01:54:10 -0400

    html changes

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -9,7 +9,7 @@ public class IndexPage extends HtmlPage {
         writer.println(
                 "<h1>Welcome!</h1>" +
                 "<p>Enter your query in the box below: " +
-                  "<form><input type=\"text\" name=\"qqq\" />" +
+                  "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
                 "</p>");
